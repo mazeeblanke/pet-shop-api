7d1ebf46d111c27e63bf70d3d8f629924a5ad466
mazinoukah <ewomaukah@yahoo.com>
2023-04-12 11:36:29 +0100

    Setup factory and seeder for brand, add sample .env.example

--- database/factories/BrandFactory.php
+++ database/factories/BrandFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Brand>
@@ -16,8 +17,13 @@ class BrandFactory extends Factory
      */
     public function definition(): array
     {
+        $title = fake()->sentence(3);
+        $slug = Str::slug($title);
+
         return [
-            //
+            'uuid' => fake()->uuid(),
+            'title' => $title,
+            'slug' => $slug
         ];
     }
 }
