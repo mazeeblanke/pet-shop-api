4293e6e7d23711289fafdea841d3f74ae218a618
mazinoukah <ewomaukah@yahoo.com>
2023-04-12 15:58:17 +0100

    Add user resource and collection

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        if (! $this->resource) {
+            return [];
+        }
+        
+        return [
+            'uuid' => $this->uuid,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'email' => $this->email,
+            'avatar' => $this->avatar,
+            'address' => $this->address,
+            'phone_number' => $this->phone_number,
+            'is_marketing' => $this->is_marketing,
+            'updated_at' => $this->updated_at,
+            'created_at' => $this->created_at,
+            'email_verified_at' => $this->email_verified_at,
+            'last_login_at' => $this->last_login_at,
+        ];
+    }
+
+    /**
+     * Get additional data that should be returned with the resource array.
+     *
+     * @return array<string, mixed>
+     */
+    public function with(Request $request): array
+    {
+        return [
+            'error' => null,
+            'errors' => [],
+            'extra' => [],
+            'success' => 1,
+        ];
+    }
+}
