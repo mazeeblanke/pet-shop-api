a646c822dfc507888c4a64fb766d961005865b9a
mazinoukah <ewomaukah@yahoo.com>
2023-04-12 14:14:51 +0100

    Set up product seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             BrandSeeder::class,
-            CategorySeeder::class
+            CategorySeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
