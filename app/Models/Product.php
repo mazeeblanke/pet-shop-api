ef5c5b11bff2fd008174ca0f650778d9cf7ae621
mazinoukah <ewomaukah@yahoo.com>
2023-04-12 14:34:16 +0100

    Add filtering capability to Products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Services\Filtering\Behaviors\HandleFilters;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, HandleFilters;
 
     protected $casts = [
         'metadata' => 'array',
