2b4b6b56fed85c0e3701da7bfd004bef60d58cfc
mazinoukah <ewomaukah@yahoo.com>
2023-04-12 11:31:35 +0100

    Add columns to brands

--- database/migrations/2023_04_12_102844_create_brands_table.php
+++ database/migrations/2023_04_12_102844_create_brands_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
+            $table->uuid()->index();
+            $table->string('title', 255);
+            $table->string('slug', 255);
             $table->timestamps();
         });
     }
