a639d0a8e4fb04157a99018b23f865d8d8019a4f
mazinoukah <ewomaukah@yahoo.com>
2023-04-12 14:12:36 +0100

    Update products migration file

--- database/migrations/2023_04_12_124624_create_products_table.php
+++ database/migrations/2023_04_12_124624_create_products_table.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->uuid();
+            $table->string('title', 255);
+            $table->float('price', 8, 2);
+            $table->string('description');
+            $table->uuid('category_uuid')->index();
             $table->timestamps();
+            $table->json('metadata')->nullable();
+            $table->softDeletes();
+
+            $table->foreign('category_uuid')
+                ->references('uuid')->on('categories');
         });
     }
 
