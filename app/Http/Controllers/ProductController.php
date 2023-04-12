10a842e356e020fb9bbe2711880331d2eae60212
mazinoukah <ewomaukah@yahoo.com>
2023-04-12 14:52:06 +0100

    Add Brand and Category to list of items to be eager loaded

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,6 +9,8 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    protected array $with = ['category', 'brand'];
+
     /**
      * Display a listing of the resource.
      *
