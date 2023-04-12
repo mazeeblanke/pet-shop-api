cc18545024b8cf9af785768b279e665968ce5c4a
mazinoukah <ewomaukah@yahoo.com>
2023-04-12 23:02:17 +0100

    Add fillable fields to Brand

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -9,4 +9,10 @@ use Illuminate\Database\Eloquent\Model;
 class Brand extends Model
 {
     use HasFactory, HandleFilters;
+
+    protected $fillable = [
+        'title',
+        'uuid',
+        'slug',
+    ];
 }
