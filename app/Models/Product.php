0ce103fa091da8f144d1c873bb0a6f717e061be3
mazinoukah <ewomaukah@yahoo.com>
2023-04-12 14:59:38 +0100

    Add Brand and Category relationships to Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Services\Filtering\Behaviors\HandleFilters;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Product extends Model
 {
@@ -13,4 +14,33 @@ class Product extends Model
     protected $casts = [
         'metadata' => 'array',
     ];
+
+    public function getBrandUuidAttribute()
+    {
+        $metadata = $this->metadata;
+
+        if (! is_array($metadata)) {
+            $metadata = json_decode($metadata, true);
+        }
+
+        return $metadata['brand'];
+    }
+
+    /**
+     * Brand Relationship
+     *
+     */
+    public function brand(): HasOne
+    {
+        return $this->hasOne(Brand::class, 'uuid', 'brand_uuid');
+    }
+
+    /**
+     * Category Relationship
+     *
+     */
+    public function category(): HasOne
+    {
+        return $this->hasOne(Category::class, 'uuid', 'category_uuid');
+    }
 }
