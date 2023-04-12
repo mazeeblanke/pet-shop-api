cacf0d14fe916d7478d7ad51b3412a025474a2a0
mazinoukah <ewomaukah@yahoo.com>
2023-04-13 00:31:13 +0100

    Add swagger docs for user create

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AuthenticateRequest;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class UserController extends AuthController
@@ -67,6 +68,98 @@ class UserController extends AuthController
         return $response;
     }
 
+
+    /**
+     * store a resource.
+     *
+     * @OA\Post(
+     *     path="/api/v1/user/create",
+     *     tags={"User"},
+     *     summary="Create a User account",
+     *     description="Users API endpoint",
+     *     operationId="user.store",
+     *     @OA\RequestBody(
+     *         description="User object",
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/x-www-form-urlencoded",
+     *             @OA\Schema(
+     *                 required={"first_name", "last_name", "email", "password", "password_confirmation", "address", "phone_number"},
+     *                 @OA\Property(
+     *                     property="first_name",
+     *                     type="string",
+     *                     description="User firstname",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="last_name",
+     *                     type="string",
+     *                     description="User lastname",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="email",
+     *                     type="string",
+     *                     description="User email",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password",
+     *                     type="string",
+     *                     description="User password"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password_confirmation",
+     *                     type="string",
+     *                     description="User password"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="avatar",
+     *                     type="string",
+     *                     description="Avatar image UUID"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="address",
+     *                     type="string",
+     *                     description="User main address"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="phone_number",
+     *                     type="string",
+     *                     description="User main phone number"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="is_marketing",
+     *                     type="string",
+     *                     description="User marketing preferences"
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="ok",
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Page not found"
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Unprocessable Entity"
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal server error"
+     *     ),
+     * )
+     */
+    public function store()
+    {
+        return parent::store();
+    }
+
     /**
      * @OA\Get(
      *     path="/api/v1/user/logout",
@@ -99,5 +192,14 @@ class UserController extends AuthController
     public function logout()
     {
         return parent::logout();
+    }
+
+    protected function getExtraData(Request $request): array
+    {
+        return [
+            'password' => bcrypt($request->password),
+            'is_admin' => 0,
+            'is_marketing' => (bool)$request->is_marketing,
+        ];
     }
 }
