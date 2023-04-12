c2cde9d0657547cb372d33e2fc13bc1b7cce75b0
mazinoukah <ewomaukah@yahoo.com>
2023-04-12 14:31:25 +0100

    Add product listing test

--- tests/Feature/Models/ProductTest.php
+++ tests/Feature/Models/ProductTest.php
@@ -2,19 +2,33 @@
 
 namespace Tests\Feature\Models;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
 {
     /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
+     * Resource list fields
+     *
+    */
+    protected array $listFields = [
+        'brand' => [
+            'slug',
+            'title',
+            'uuid'
+        ],
+        'category' => [
+            'slug',
+            'title',
+            'uuid'
+        ],
+        'category_uuid',
+        'description',
+        'metadata' => [
+            'brand',
+            // 'image'
+        ],
+        'price',
+        'title',
+        'uuid'
+    ];
 }
