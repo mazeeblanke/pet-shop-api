678bab2f685df7e3288b970ac4ea375df9106e7f
mazinoukah <ewomaukah@yahoo.com>
2023-04-13 03:23:25 +0100

    Add admin user listing controller method and generate swagger docs

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\AuthController;
 use App\Http\Requests\AuthenticateRequest;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class UserController extends AuthController
@@ -100,5 +101,97 @@ class UserController extends AuthController
     public function logout()
     {
         return parent::logout();
+    }
+
+    /**
+     * Display user listing.
+     *
+     *   @OA\Get(
+     *     path="/api/v1/admin/user-listing",
+     *     tags={"Admin"},
+     *     summary="List all users",
+     *     description="Admin API endpoint",
+     *     operationId="admin.index",
+     *     security={{ "bearerAuth": {} }},
+     *     @OA\Parameter(
+     *         name="page",
+     *         in="query",
+     *         description="",
+     *         required=false,
+     *         @OA\Schema(
+     *             default="",
+     *             type="integer",
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="limit",
+     *         in="query",
+     *         description="",
+     *         required=false,
+     *         explode=false,
+     *         @OA\Schema(
+     *             default="",
+     *             type="integer",
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="sortBy",
+     *         in="query",
+     *         description="",
+     *         required=false,
+     *         explode=true,
+     *         @OA\Schema(
+     *             default="",
+     *             type="string",
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="email",
+     *         in="query",
+     *         description="",
+     *         required=false,
+     *         explode=true,
+     *         @OA\Schema(
+     *             default="",
+     *             type="string",
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="desc",
+     *         in="query",
+     *         description="",
+     *         required=false,
+     *         explode=true,
+     *         @OA\Schema(
+     *             default="",
+     *             type="boolean",
+     *             enum={"true", "false"},
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="ok",
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Page not found"
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Unprocessable Entity"
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal server error"
+     *     ),
+     * )
+     */
+    public function index(Request $request)
+    {
+        return parent::index($request);
     }
 }
