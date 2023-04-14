1333e094df0d20b2fd2f91e4d091e8b303e85acd
mazinoukah <ewomaukah@yahoo.com>
2023-04-14 01:09:09 +0100

    Create converter factory

---
+++ packages/mazi/currency-converter/src/ConverterFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Mazi\CurrencyConverter;
+
+use Exception;
+use GuzzleHttp\ClientInterface;
+use Illuminate\Foundation\Application;
+use Mazi\CurrencyConverter\Contracts\Parser;
+use Mazi\CurrencyConverter\Drivers\EuroBankDriver;
+
+class ConverterFactory
+{
+    /**
+     * Supported drivers
+     *
+     * @var array
+     */
+    protected const DRIVERS = [
+        'eurobankDriver' => EuroBankDriver::class,
+    ];
+
+    public static function make(string $driverName, ClientInterface $http, Application $app)
+    {
+        // get driver from list of supported drivers
+        if (!isset(static::DRIVERS[$driverName])) {
+            throw new Exception("{$driverName} is not a supported driver.");
+        }
+
+        // initialize it with dependencies
+        $driverClass = static::DRIVERS[$driverName];
+
+        $parser = $app->make(Parser::class);
+
+        // return instance of Converter class
+        return new Converter(new $driverClass($http, $parser));
+    }
+
+    /**
+     * List available drivers.
+     *
+     * @return array
+     */
+    public function drivers(): array
+    {
+        return self::DRIVERS;
+    }
+}
