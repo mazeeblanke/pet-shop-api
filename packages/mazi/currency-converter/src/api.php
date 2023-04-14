c1c69f6e15b5ef4b5b895f8105ece2b439d54e05
mazinoukah <ewomaukah@yahoo.com>
2023-04-14 01:01:12 +0100

    Update route definition

--- packages/mazi/currency-converter/src/api.php
+++ packages/mazi/currency-converter/src/api.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Mazi\CurrencyConverter\Http\Controllers\Controller;
 
 Route::group(['prefix' => 'api/v1'], function() {
-    Route::get('convert-currency/{amount}/{currency}', [Controller::class, 'convert']);
+    Route::get('convert-currency', [Controller::class, 'convert']);
 });
 
