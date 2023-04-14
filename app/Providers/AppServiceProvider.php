4a92ec4faa3198f84bf2303021a8086040b19d93
mazinoukah <ewomaukah@yahoo.com>
2023-04-14 01:03:34 +0100

    Register currency converter in the AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,14 @@
 
 namespace App\Providers;
 
+use Illuminate\Contracts\Cache\Repository;
 use Illuminate\Support\ServiceProvider;
+use Mazi\CurrencyConverter\Contracts\Converter as ContractsConverter;
+use Mazi\CurrencyConverter\Contracts\ConverterDriver;
+use Mazi\CurrencyConverter\Contracts\Parser;
+use Mazi\CurrencyConverter\Converter;
+use Mazi\CurrencyConverter\Drivers\EuroBankDriver;
+use Mazi\CurrencyConverter\Parsers\XMLParser;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,6 +18,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        $this->registerCurrencyConverter();
     }
 
     /**
@@ -18,5 +26,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+    }
+
+    private function registerCurrencyConverter(): void
+    {
+        $this->app->bind(
+            Parser::class,
+            XMLParser::class
+        );
+
+        $this->app->singleton(ConverterDriver::class, function () {
+            return new EuroBankDriver(
+                new \GuzzleHttp\Client(),
+                $this->app->make(Parser::class),
+                $this->app->make(Repository::class)
+            );
+        });
+
+        $this->app->singleton(ContractsConverter::class, function() {
+            return new Converter(
+                $this->app->make(ConverterDriver::class
+            ));
+        });
     }
 }
