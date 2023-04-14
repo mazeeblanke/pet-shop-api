d6e1f7ff30de8cd389ea80296a1646f61257fffd
mazinoukah <ewomaukah@yahoo.com>
2023-04-14 02:47:23 +0100

    Implement symbol test

---
+++ packages/mazi/currency-converter/tests/SymbolTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Mazi\CurrencyConverter\Symbol;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * SymbolTest
+ */
+class SymbolTest extends TestCase
+{
+  /** @test */
+  public function can_get_all_symbols()
+  {
+    $this->assertCount(167, Symbol::all());
+  }
+
+  /** @test */
+  public function can_get_a_symbol_name()
+  {
+    $this->assertEquals('Albanian Lek', Symbol::name(Symbol::ALL));
+    $this->assertEquals('Euro', Symbol::name(Symbol::EUR));
+  }
+
+  /** @test */
+  public function can_get_a_list_of_all_symbols()
+  {
+    $this->assertCount(167, Symbol::names());
+    $this->assertEquals('Albanian Lek', Symbol::names()[Symbol::ALL]);
+    $this->assertEquals('Euro', Symbol::names()[Symbol::EUR]);
+  }
+}
