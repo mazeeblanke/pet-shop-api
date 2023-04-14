bac1fda40e811f46d121587303076ac3fb33178c
mazinoukah <ewomaukah@yahoo.com>
2023-04-14 01:08:28 +0100

    Implement error handling

--- packages/mazi/currency-converter/src/Drivers/EuroBankDriver.php
+++ packages/mazi/currency-converter/src/Drivers/EuroBankDriver.php
@@ -2,10 +2,12 @@
 
 namespace Mazi\CurrencyConverter\Drivers;
 
-use Exception;
 use GuzzleHttp\ClientInterface;
+use Illuminate\Contracts\Cache\Repository;
 use Mazi\CurrencyConverter\Contracts\ConverterDriver;
 use Mazi\CurrencyConverter\Contracts\Parser;
+use Mazi\CurrencyConverter\Exceptions\CurrencyNotFound;
+use Mazi\CurrencyConverter\Exceptions\UnsupportedCurrencySymbol;
 use Mazi\CurrencyConverter\Symbol;
 
 class EuroBankDriver implements ConverterDriver
@@ -56,6 +58,13 @@ class EuroBankDriver implements ConverterDriver
     protected Parser $parser;
 
     /**
+     * Cache service
+     *
+     * @var [type]
+     */
+    protected Repository $cache;
+
+    /**
      * List of symbols supported by driver
      *
      * @var array
@@ -92,10 +101,11 @@ class EuroBankDriver implements ConverterDriver
         Symbol::ZAR
     ];
 
-    public function __construct(ClientInterface $http, Parser $parser)
+    public function __construct(ClientInterface $http, Parser $parser, Repository $cache)
     {
         $this->http = $http;
         $this->parser = $parser;
+        $this->cache = $cache;
     }
 
     /**
@@ -108,10 +118,22 @@ class EuroBankDriver implements ConverterDriver
      */
     public function process($targetCurrency, $amount): string
     {
-        // check if target curreny is supported
+        $targetCurrency = strtoupper($targetCurrency);
+
+        // check if target currency is supported
         if(!in_array($targetCurrency, $this->supportedSymbols)) {
-            // throw error
+            throw new UnsupportedCurrencySymbol(
+                'Driver does not support symbol ' . $targetCurrency
+            );
         }
+
+        $cachedData = $this->cache->get(
+            config('cur-converter.cache-key'),
+            []
+        );
+
+        $this->lastDate = $cachedData['lastRefresh'] ?? '';
+        $this->rates = $cachedData['rates'] ?? [];
 
         // if not already set and still valid
         if (!isset($this->rates) || !$this->isValid()) {
@@ -149,6 +171,15 @@ class EuroBankDriver implements ConverterDriver
             $data['Cube']['Cube']['Cube']
         );
 
+        $this->cache->put(
+            config('cur-converter.cache-key'),
+            [
+                'rates' => $this->rates,
+                'lastRefresh' => $this->lastDate
+            ],
+            config('cur-converter.cache-time')
+        );
+
         return $this->rates;
     }
 
@@ -156,12 +187,14 @@ class EuroBankDriver implements ConverterDriver
     {
         $currency = array_filter(
             $this->rates,
-            fn($arr)=> $arr['currency'] == strtoupper($targetCurrency)
+            fn($arr) => $arr['currency'] == $targetCurrency
         );
 
         // if no results
         if (count($currency) == 0) {
-            // throw error
+            throw new CurrencyNotFound(
+                'Unable to find currency ' . $targetCurrency
+            );
         }
 
         return reset($currency);
