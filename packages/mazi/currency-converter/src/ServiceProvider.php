fcf61380dd955ab1fc85841dba79dba3da88c5e5
mazinoukah <ewomaukah@yahoo.com>
2023-04-14 01:09:38 +0100

    Update service controller

--- packages/mazi/currency-converter/src/ServiceProvider.php
+++ packages/mazi/currency-converter/src/ServiceProvider.php
@@ -19,5 +19,12 @@ class ServiceProvider extends BaseServiceProvider
     public function boot(): void
     {
         $this->loadRoutesFrom(__DIR__.'/api.php');
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/cur-converter.php',
+            'cur-converter'
+        );
+        $this->publishes([
+            __DIR__.'/../config/cur-converter.php' => config_path('cur-converter.php'),
+        ]);
     }
 }
