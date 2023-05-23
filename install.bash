6bde608bbd2dc21e3396e9a4cfffcd98e4d1eed0
mazinoukah <ewomaukah@yahoo.com>
2023-05-23 09:29:31 +0100

    Remove traces to mysql test container

--- install.bash
+++ install.bash
@@ -23,24 +23,11 @@ until docker-compose exec pet-shop-db mysqladmin ping --silent; do
 done
 echo "MySQL DEV server is now available!"
 
-echo "Waiting for TEST MySQL server to start up..."
-until docker-compose exec pet-shop-db-test mysqladmin ping --silent; do
-    sleep 10
-done
-
-echo "MySQL TEST server is now available!"
-
 # Create MySQL DEV database securely
 docker-compose exec pet-shop-db bash -c "\
         source .env && \
         export MYSQL_PWD='${DB_PASSWORD}' && \
         mysql -u root -e 'CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};'"
-
-# Create MySQL TEST database securely
-docker-compose exec pet-shop-db-test bash -c '\
-        source .env.testing && \
-        export MYSQL_PWD="${DB_PASSWORD}" && \
-        mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};"'
 
 # Generate app key
 docker-compose exec pet-shop-api bash -c "php artisan key:generate"
@@ -56,9 +43,7 @@ echo "Running PET SHOP Tests!"
 # php artisan test
 
 # Run test in container
-docker-compose exec pet-shop-api bash -c 'source .env.testing && php artisan test'
-
-
+docker-compose exec pet-shop-api bash -c 'php artisan test'
 
 # # Run locally
 # echo "Navigate to Currency converter package!"
