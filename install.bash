d410d3eecbc330e9827c050b4d8b3138c66d1437
mazinoukah <ewomaukah@yahoo.com>
2023-05-23 10:36:58 +0100

    Reformat bash commands with single quotes to fix bug of wrong env variables

--- install.bash
+++ install.bash
@@ -17,39 +17,37 @@ composer install
 docker-compose up -d
 
 # Wait for MySQL server to start up
-echo "Waiting for DEV MySQL server to start up..."
+echo 'Waiting for DEV MySQL server to start up...'
 until docker-compose exec pet-shop-db mysqladmin ping --silent; do
     sleep 10
 done
-echo "MySQL DEV server is now available!"
+echo 'MySQL DEV server is now available!'
 
 # Create MySQL DEV database securely
-docker-compose exec pet-shop-db bash -c "\
-        source .env && \
-        export MYSQL_PWD='${DB_PASSWORD}' && \
-        mysql -u root -e 'CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};'"
+docker-compose exec pet-shop-db bash -c '\
+        source /data/.env && \
+        export MYSQL_PWD="${DB_PASSWORD}" && \
+        mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};"'
+
+
+# Create MySQL Test database securely
+docker-compose exec pet-shop-db bash -c '\
+        source /data/.env.testing && \
+        export MYSQL_PWD="${DB_PASSWORD}" && \
+        echo ${DB_DATABASE} && \
+        mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};"'
 
 # Generate app key
-docker-compose exec pet-shop-api bash -c "php artisan key:generate"
+docker-compose exec pet-shop-api bash -c 'php artisan key:generate'
 
 # Run database migrations and seeds
-docker-compose exec pet-shop-api bash -c "php artisan migrate --seed"
-
+docker-compose exec pet-shop-api bash -c 'php artisan migrate --seed'
 
 # Run Pet Shop tests
-echo "Running PET SHOP Tests!"
-
-# Run tests locally
-# php artisan test
+echo 'Running PET SHOP Tests!'
 
 # Run test in container
 docker-compose exec pet-shop-api bash -c 'php artisan test'
-
-# # Run locally
-# echo "Navigate to Currency converter package!"
-# cd packages/mazi/currency-converter && composer install
-# echo "Running Currency Converter Tests!"
-#  ./vendor/bin/phpunit
 
 # Run in container
 docker-compose exec pet-shop-api bash -c 'cd packages/mazi/currency-converter && \
